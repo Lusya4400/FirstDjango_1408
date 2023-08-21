d1e24b9c53d8dc9e818e4ed1b42b080044922a85
usasheva_ludmila <luda-a@rambler.ru>
2023-08-21 18:22:31 +0300

    mod_4

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
+class Color(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __repr__(self):
+        return f'Color({self.name})'
+
 # Create your models here.
 class Item(models.Model):
     name  = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField()
     descript = models.CharField(max_length=200)
+    colors = models.ManyToManyField(to=Color)
 
     def __repr__(self):
         return f'Item({self.name}, {self.brand}, {self.count})' 
